55e9e527f595cc0d720df712583327e26d958a8a
Denis Kudakov <denn88kudakov@yandex.ru>
2022-12-11 14:10:04 +0300

    Create Dockerfile

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -2,17 +2,10 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
@@ -71,17 +64,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='name'),
+        'USER': os.getenv('POSTGRES_USER', default='user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='password'),
+        'HOST': os.getenv('DB_HOST', default='host'),
+        'PORT': os.getenv('DB_PORT', default='port')
     }
-    # 'default': {
-    #     'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-    #     'NAME': os.getenv('DB_NAME', default='name'),
-    #     'USER': os.getenv('POSTGRES_USER', default='user'),
-    #     'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='password'),
-    #     'HOST': os.getenv('DB_HOST', default='host'),
-    #     'PORT': os.getenv('DB_PORT', default='port')
-    # }
 }
 
 
